058be01ba3dba1c797d23d9412cb0b7c3619268f
Paul Denisov <paul.denisov@gmail.com>
2016-03-09 23:42:54 -0700

    6-8 lessons code has been added

---
+++ 678_lessons_if_float_lcd/area_float.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a,b,c;
+	float p,S;
+    const float PI = 3.14159;
+
+	printf("Длины сторон треугольника: ");
+	scanf("%d%d%d", &a,&b,&c);
+	p = (a+b+c)/2;
+	S = sqrt(p*(p-a)*(p-b)*(p-c));
+	printf("P = %d; S = %.2f\n\n", (a+b+c), S);
+
+	printf("Ширина и высота прямоугольника: ");
+	scanf("%d%d", &a,&b);
+	printf("P = %d; S = %d\n\n", (a+b)*2, a*b);
+
+	printf("Радиус круга: ");
+	scanf("%d", &a);
+	printf("P = %.2f; S = %.2f\n", 2*PI*a, PI*pow(a,2));
+
+    return 0;
+}
---
+++ 678_lessons_if_float_lcd/triangle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    printf("a=");
+    scanf("%d", &a);
+    printf("b=");
+    scanf("%d", &b);
+    printf("c=");
+    scanf("%d", &c);
+
+    if (a+c<b)
+    {
+        printf("impossible triangle");
+    }
+    else if (a+b<c)
+    {
+        printf("impossible triangle");
+    }
+    else if (b+c<a)
+    {
+        printf("impossible triangle");
+    }
+    else
+    {
+        printf("possible");
+        if (a==b && a==c)
+        {
+            printf(" equilateral");
+        }
+        else if (a!=b && b!=c && a!=c)
+        {
+            printf(" obtuse");
+        }
+        else
+        {
+            printf(" isoceles");
+        }
+    }
+    return 0;
+}
