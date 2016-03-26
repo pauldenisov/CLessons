326654ac669901ddba2d19705b8286d2dd37f055
Paul Denisov <paul.denisov@gmail.com>
2016-03-26 00:10:37 -0600

    Loop operators, part 1

---
+++ 11_loop1/11_loop_p1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c=1;
+    int s=1;
+    while ( c <= 10 ) {
+        while ( s <= c ) {
+            printf("* ");
+            s=s+1;
+        }
+        printf("\n");
+        c=c+1;
+        s=1;
+    }
+    printf("C is bigger than 10, C=%d",c);
+    return 0;
+}
